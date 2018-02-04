fc5ec8ccddffa3fdb5333504874a2df58d40c4a4
Ramiro Morales <cramm0@gmail.com>
2018-02-04 20:22:43 -0300

    WIP: We can write translation .po files.

--- tools/intl.py
+++ tools/intl.py
@@ -1,17 +1,31 @@
 # intl.py
+from __future__ import print_function
 
+import datetime
 import fnmatch
+import io
 import logging
 import os
-import pprint
+import pprint  # noqa
 import sys
+from optparse import OptionParser
 
 import polib
 
+__version__ = '0.1'
 
 TRANSLATION_FUNCTION = 'msg_hash_to_str'
 TRANSLATION_MACRO = 'MSG_HASH'
 
+ORIGINAL_LITERAL_FILES = ('us', 'lbl')
+
+RA_LOCALE_NAME_MAP = {
+    'pt_br': 'pt_BR',
+    'pt_pt': 'pt_PT',
+    'us': 'en_US',
+}
+
+OUTPUT_DIR = 'intl'
 
 class SyntaxError(Exception):
     pass
@@ -26,15 +40,18 @@ class DuplicateLiteral(Exception):
 
 
 def enumerate_files():
-    top = '.'
+    top = os.path.curdir
     matches = []
     for root, dirnames, filenames in os.walk(top):
         if root == top and 'intl' in dirnames:
             ndx = dirnames.index('intl')
             del dirnames[ndx]
-        # print root, dirnames, filenames
+        # print(root, dirnames, filenames)
         for filename in fnmatch.filter(filenames, '*.c'):
-            matches.append(os.path.join(root, filename))
+            full_path = os.path.join(root, filename)
+            if full_path.startswith(os.path.curdir + os.path.sep):
+                full_path = full_path[2:]
+            matches.append(full_path)
     return matches
 
 
@@ -73,48 +90,6 @@ def find_token(y, x, lines, search_text):
     raise SyntaxError("Reached end of file and didn't find matching closing parenthesis.", y, lines[y])
 
 
-# def extract_symbols(symbols, filename, file_object, found_list, search_text):
-#     file_object.seek(0)
-#     contents = file_object.read()
-#     lines = contents.splitlines()
-#     for y, x in found_list:
-#         line = lines[y][:]
-#         temp_lineno = y
-#         begin = x + len(search_text)
-#         found_opening_parens = False
-#         while True:
-#             if begin == len(line):
-#                 temp_lineno += 1
-#                 line += lines[temp_lineno][:]
-#             if line[begin] not in (' ', '\t'):
-#                 if line[begin] == '(':
-#                     found_opening_parens = True
-#                 break
-#             begin += 1
-#         if not found_opening_parens:
-#             continue
-#         parens_cnt = 1
-#         end = begin + 1
-#         while True:
-#             if end == len(line):
-#                 temp_lineno += 1
-#                 line += lines[temp_lineno][:]
-#             if line[end] == '(':
-#                 parens_cnt += 1
-#             elif line[end] == ')':
-#                 parens_cnt -= 1
-#                 if not parens_cnt:
-#                     break
-#             end += 1
-#         if not parens_cnt:
-#             parens_contents = line[begin:end]
-#             parens_contents = parens_contents.strip(' \t()')
-#             if parens_contents.isupper():
-#                 symbols.setdefault(parens_contents, []).append((filename, y))
-#         else:
-#             logging.error("%s:%d: Reached end of file and didn't find matching closing parenthesis.", filename, y)
-
-
 def extract_symbols(symbols, filename, file_object, found_list, search_text):
     file_object.seek(0)
     contents = file_object.read()
@@ -129,12 +104,16 @@ def extract_symbols(symbols, filename, file_object, found_list, search_text):
             return
         else:
             text = text.strip(' \t()')
+            # TODO: Check what's the RA policy Re: Casing of these macro names
             if text.isupper():
                 symbols.setdefault(text, []).append((filename, y + 1))  # Our line index is 0-based
 
 
 def find_translation_calls_in_file(symbols, filename):
     try:
+        # TODO: Hot to best handle inconsistent/undefined encoding of RA source files
+        # e.g. wii/libogc/libogc/console_font_8x16.c at offset 89868
+        # with io.open(filename, 'rt', encoding='ascii', newline=None) as f:
         with open(filename, 'rU') as f:
             found = []
             for line_number, line in enumerate(f):
@@ -168,57 +147,174 @@ def extract_translations(original_literals, filename, file_object, found_list, s
                 literal = parts[1].strip(' \t"')
                 if symbol in original_literals:
                     raise DuplicateLiteral(symbol, y)
-                original_literals[symbol] = {'lineno': y + 1, 'literal': literal}  # Our line index is 0-based
+                original_literals[symbol] = {
+                    'file': filename,
+                    'lineno': y + 1,  # Our line index is 0-based
+                    'literal': literal
+                }
             else:
-                raise SyntaxWarning("Malformed MSG_HASH call.", filename, y)
+                raise SyntaxWarning("Malformed MSG_HASH call.", filename, y + 1)
 
 
-def extract_translations_from_msg_hash_xx_h(locale, original_literals):
+def extract_translations_from_msg_hash_xx_h(locale):
+    literals = {}
     filename = os.path.join('.', 'intl', 'msg_hash_%s.h' % locale)
     try:
-        with open(filename, 'rU') as f:
+        # with open(filename, 'rU') as f:
+        with io.open(filename, 'rt', encoding='utf-8', newline=None) as f:
             found = []
             for line_number, line in enumerate(f):
                 column = line.find(TRANSLATION_MACRO)
                 if column != -1:
                     found.append((line_number, column))
             if found:
-                extract_translations(original_literals, filename, f, found, TRANSLATION_MACRO)
+                extract_translations(literals, filename, f, found, TRANSLATION_MACRO)
 
     except IOError as e:
         logging.error(e)
+    return literals
+
+
+def common(symbols, original_literals):
+    filenames = enumerate_files()
+    # print(filenames)
+    for filename in filenames:
+        find_translation_calls_in_file(symbols, filename)
+    # pprint.pprint(symbols)
+    for orig_file in ORIGINAL_LITERAL_FILES:
+        original_literals.update(extract_translations_from_msg_hash_xx_h(orig_file))
+    # pprint.pprint(original_literals)
+
+
+def check(options):
+
+    def key(entry):
+        return (original_literals[entry]['file'], original_literals[entry]['lineno'])
+
+    if options.output:
+        logging.error("check action doesn't need the -o/--output option")
+        return 2
+    symbols = {}
+    original_literals = {}
+    common(symbols, original_literals)
+    for entry in sorted(original_literals, key=key):
+        symbols.pop(entry, None)
+    if symbols:
+        logging.warning("The following Retroch translatable literal IDs don't have an english original literal defined:")
+        for k in symbols:
+            logging.warning("\t%s (used in %s)", k, ', '.join('%s:%d' % info for info in symbols[k]))
+    return 0
+
+
+def updatepo(options):
+
+    def key(entry):
+        return (original_literals[entry]['file'], original_literals[entry]['lineno'])
+
+    locale = RA_LOCALE_NAME_MAP.get(options.locale, options.locale)
+    if not options.output:
+        # logging.error("you ned to specify the -o/--output option when using the updatepo action")
+        # return 2
+        if options.locale == 'us':
+            output = os.path.join(OUTPUT_DIR, 'us.pot')
+        else:
+            output = os.path.join(OUTPUT_DIR, '%s.po' % options.locale)
+            if os.path.exists(output):
+                # TODO: Handle actually updating an existing .po file
+                logging.critical("%s exists. refusing to overwite it for now", output)
+                return 3
+    elif options.output == '-':
+        output = 'CON' if sys.platform == 'win32' else '/dev/stdout'
+    else:
+        output = options.output
+
+    symbols = {}
+    original_literals = {}
+    common(symbols, original_literals)
+    # TODO: Pass wrapwidth=160?
+    pof = polib.POFile()
+    utcnow = datetime.datetime.utcnow().replace(second=0, microsecond=0).isoformat(' ') + '+0000'
+    pof.metadata = {
+        'Project-Id-Version': 'RetroArch',
+        # 'Report-Msgid-Bugs-To': 'you@example.com',
+        # 'Last-Translator': 'you <you@example.com>',
+        # 'Language-Team': 'English <yourteam@example.com>',
+        'Language': RA_LOCALE_NAME_MAP.get(options.locale, options.locale),
+        'MIME-Version': '1.0',
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Content-Transfer-Encoding': '8bit',
+    }
+    if options.locale == 'us':
+        pof.metadata.update(
+            {'POT-Creation-Date': utcnow}
+        )
+        for entry in sorted(original_literals, key=key):
+            symbol_def = symbols.get(entry)
+            if symbol_def is not None:
+                # msgid = polib.unescape(original_literals[entry]['literal'])
+                msgid = original_literals[entry]['literal']
+                # TODO: Enhance these heuristics
+                flags = ['c-format'] if '%' in msgid else []
+                po_entry = polib.POEntry(
+                    msgid=msgid,
+                    msgstr='',
+                    occurrences=symbol_def,
+                    # comment=entry,
+                    msgctxt=entry,
+                    flags=flags
+                )
+                pof.append(po_entry)
+    else:
+        pof.metadata.update(
+            {'PO-Revision-Date': utcnow}
+        )
+        existing_translations = extract_translations_from_msg_hash_xx_h(options.locale)
+        # pprint.pprint(existing_translations)
+        for entry in sorted(original_literals, key=key):
+            symbol_def = symbols.get(entry)
+            if symbol_def is not None:
+                # msgid = polib.unescape(original_literals[entry]['literal'])
+                msgid = original_literals[entry]['literal']
+                # TODO: Enhance these heuristics
+                flags = ['c-format'] if '%' in msgid else []
+                translated_def = existing_translations.get(entry)
+                msgstr = '' if translated_def is None else translated_def['literal']
+                po_entry = polib.POEntry(
+                    msgid=msgid,
+                    msgstr=msgstr,
+                    occurrences=symbol_def,
+                    # comment=entry,
+                    msgctxt=entry,
+                    flags=flags
+                )
+                pof.append(po_entry)
+    pof.save(output)
+    return 0
 
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    filenames = enumerate_files()
-    # print filenames
-    symbols = {}
-    original_literals = {}
-    for filename in filenames:
-        find_translation_calls_in_file(symbols, filename)
-    # pprint.pprint(symbols)
-    extract_translations_from_msg_hash_xx_h('us', original_literals)
-    # pprint.pprint(original_literals)
-    pof = polib.POFile()
-    for entry in sorted(original_literals, key=lambda e: original_literals[e]['lineno']):
-        # pprint.pprint("%r %r" % (entry, original_literals[entry]))
-        symbol_def = symbols.pop(entry, None)
-        if symbol_def is not None:
-            po_entry = polib.POEntry(
-                msgid=polib.unescape(original_literals[entry]['literal']),
-                msgstr='',
-                occurrences=symbol_def,
-                comment=entry,
-                flags=['c-format']
-            )
-            pof.append(po_entry)
-    # pof.save('/dev/stdout')
-    # print pof.percent_translated()
-    # pprint.pprint(symbols)
-    return 0
+    parser = OptionParser(
+        usage='%prog [options] [command]',
+        version='%prog ' + __version__
+    )
+    parser.add_option('-l', '--locale', default='us', help='locale name to work with')
+    parser.add_option('-o', '--output', help='PO file to write to')
+    options, args = parser.parse_args(args=argv[1:])
+    if not args:
+        action = 'check'
+    elif len(args) == 1:
+        action = args[0]
+    else:
+        parser.error('extraneous command line options')
+    if action == 'check':
+        return check(options)
+    elif action == 'updatepo':
+        return updatepo(options)
+    else:
+        parser.error('unknown action: \'%s\'' % action)
 
 
 if __name__ == '__main__':
